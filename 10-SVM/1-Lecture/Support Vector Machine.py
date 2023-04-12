48d103af5ef0901e597cf12ff89d30957acbc8bd
Max Alekseyev <maxale@users.noreply.github.com>
2023-04-11 20:03:16 -0400

    using mlxtend

--- 10-SVM/1-Lecture/Support Vector Machine.py
+++ 10-SVM/1-Lecture/Support Vector Machine.py
@@ -18,7 +18,7 @@ y_combined = np.hstack((y_train, y_test))
 #-----------------------------------------------------------------------------
 
 from sklearn.linear_model import LogisticRegression
-import plot_decision_regions as pp
+import mlxtend.plotting as pp
 import matplotlib.pyplot as plt
 
 #-----------------------------------------------------------------------------
@@ -29,7 +29,7 @@ from sklearn.svm import SVC
 svm = SVC(kernel='linear', C=1.0, random_state=0)
 svm.fit(X_train_std, y_train)
 
-pp.plot_decision_regions(X_combined_std,y_combined, classifier=svm,test_idx=range(105,150))
+pp.plot_decision_regions(X_combined_std, y_combined, clf=svm, X_highlight=X_test_std)
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
