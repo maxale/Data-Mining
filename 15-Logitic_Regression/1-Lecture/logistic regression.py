de6a8c1db795df8ce08a915539873e9c4136c932
Max Alekseyev <maxale@users.noreply.github.com>
2024-04-23 22:23:47 -0400

    Update logistic regression.py
    use of mlxtend for plot_decision_regions

--- 15-Logitic_Regression/1-Lecture/logistic regression.py
+++ 15-Logitic_Regression/1-Lecture/logistic regression.py
@@ -18,11 +18,12 @@ y_combined = np.hstack((y_train, y_test))
 #-----------------------------------------------------------------------------
 
 from sklearn.linear_model import LogisticRegression
-import plot_decision_regions as pp
+import mlxtend.plotting as pp
 import matplotlib.pyplot as plt
+
 lr = LogisticRegression(C=1000.0, random_state=0)
 lr.fit(X_train_std, y_train)
-pp.plot_decision_regions(X_combined_std,y_combined, classifier=lr,test_idx=range(105,150))
+pp.plot_decision_regions(X_combined_std,y_combined, clf=lr, X_highlight=X_test_std)
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
